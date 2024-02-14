c1c4163e1858bbd6968ee6f1882bf12ec6e30f58
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-02-15 00:01:49 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl text-center text-red-700">
+      Welcome to Vite With Tailwind
+    </h1>
+  );
+};
+
+export default App;
